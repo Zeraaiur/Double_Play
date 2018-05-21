a7d5ec77aca1184fe043c64d39f9f69dc6861723
Neal Sutaria <nealsutaria99@gmail.com>
2018-05-21 17:39:36 -0400

    changed bookings controller method to add current_user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,6 +9,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @item = Item.find(params[:item_id])
     @booking.item = @item
+    @booking.user = current_user
     if @booking.save
       redirect_to item_path(@item)
     else
