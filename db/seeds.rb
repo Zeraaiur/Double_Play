5af247b48c360ece4b44f275a6cfd50464c25dbb
David Poisson <zeraaiur@hotmail.com>
2018-05-21 17:43:43 -0400

    Update to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dude = User.create!(email: "test@test.ca", address: "sss", password:"password", username:"test", last_name:"dude", first_name:"gab")
+
+Item.create!(name: "Glove", description:"whatever", price: 10, user: dude)
+
