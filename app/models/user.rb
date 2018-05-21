ca1ad5092c3be7ad3d1b95762dda1ce8b5b8fe2e
Ben Calef <bencalef@gmail.com>
2018-05-21 16:21:17 -0400

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :items
+  has_many :bookings
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :address, presence: true
+
 end
