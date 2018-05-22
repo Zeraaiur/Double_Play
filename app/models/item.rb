90fb4a31fe0dbc5fa4496c97d4e87277a4507087
Neal Sutaria <nealsutaria99@gmail.com>
2018-05-22 14:03:34 -0400

    cloudinary

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,5 @@ class Item < ApplicationRecord
   validates :name, presence:true
   validates :description, presence: true
   validates :price, presence: true
+  mount_uploader :photo, PhotoUploader
 end
