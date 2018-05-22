9f9d2b7f412401b44358d0f369060d9d41fb80c7
David Poisson <zeraaiur@hotmail.com>
2018-05-22 15:09:55 -0400

    login logout and manage account buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :bookings
-  has_many :items
+  has_many :bookings, dependent: :destroy
+  has_many :items, dependent: :destroy
   validates :last_name, presence:true
   validates :username, presence: true
   validates :last_name, presence: true
