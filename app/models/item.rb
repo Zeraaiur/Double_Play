56c3aecc904f9ead5c1ca9b100ba55d8700ab645
Neal Sutaria <nealsutaria99@gmail.com>
2018-05-24 14:09:02 -0400

    search

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,10 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   mount_uploader :photo, PhotoUploader
+  include PgSearch
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
