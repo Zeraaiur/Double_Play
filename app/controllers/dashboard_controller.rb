2dbb946cdbf7ff0ac9d8c720f8f785c49953ca95
Brett Middleton <brett01101101@gmail.com>
2018-05-23 23:36:49 -0400

    dashboard functional

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    @user_items = Item.where(:user_id => current_user.id)
     @bookings = Booking.where(:user_id => current_user.id)
+    # Need to update DB for boolean of rented
+    # @rentals = Item.where(:user_id => current_user.id && :rented => false)
   end
-
 end
